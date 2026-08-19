e1d1ed040bef514506c160e06a67da32db137964
sanhith vandara <sanhith1211@gmail.com>
2026-08-19 16:12:04 -0400

    Add MathMethodsDemo class with math method examples
    Implement MathMethodsDemo to demonstrate various math methods.

---
+++ Honors_Java_25-26/mathmethoddemo.java
@@ -0,0 +1,16 @@
+// Sanhith Vandara 
+// Math Methods
+// Mrs. Scullin 
+// Java 9  
+
+import java.util.*;
+
+public class MathMethodsDemo {
+    public static void main(String[] args) {
+// print 
+        System.out.println(Math.signum(4));       
+        System.out.println(Math.scalb(4, 8));   
+        System.out.println(Math.sin(5.3));         
+
+    }
+}
