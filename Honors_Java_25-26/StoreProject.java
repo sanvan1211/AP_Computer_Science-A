f6c17ffafbe11b35e3ac660aee52125bbb599569
sanhith vandara <sanhith1211@gmail.com>
2026-08-19 16:09:47 -0400

    Update StoreProject.java

--- Honors_Java_25-26/StoreProject.java
+++ Honors_Java_25-26/StoreProject.java
@@ -27,3 +27,38 @@ public class Main {
     
     }
   }
+
+public class Store {
+  // declare instance variables 
+  String productType;
+  double price; 
+  
+  // constructor assigns values to the instance variables 
+  public Store(String product, double initialPrice) {
+    productType = product; 
+    price = initialPrice; 
+  }  
+  
+  // creating method called greetCustomer that does not return anything (void)
+  public void greetCustomer(String customer) {
+    System.out.println("Welcome to my store, " + customer + "!");
+  }
+
+  // create method that passes a price and calculates a new price 
+  public void increasePrice(double priceToAdd) {
+    double newPrice = price + priceToAdd;
+    price = newPrice;
+    System.out.println("New price for item $" + price);
+  }
+
+  // creating a method not passing a parameter and returns double
+  public double getTax() {
+    double totalPrice = price + price * 0.06;
+    return totalPrice; 
+  }
+
+  // toString method built in method that returns a string value 
+  public String toString() {
+    return "My store sells " + productType + " at a fixed price of $" + price;
+  }
+}
