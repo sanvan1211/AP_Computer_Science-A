32ad715c456de298a52d1c89a533367c47e96985
sanhith vandara <sanhith1211@gmail.com>
2026-08-19 16:12:35 -0400

    Add recursion.java with coffee drinking simulation

---
+++ Honors_Java_25-26/recursion.java
@@ -0,0 +1,13 @@
+//Sanhith Vandara
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int cupsOfCoffee = 1; 
+      while(cupsOfCoffee <= 100){
+        System.out.print("\nJoey drinks a cup of coffee #" +cupsOfCoffee);
+        cupsOfCoffee++;
+      }
+  }
+}
