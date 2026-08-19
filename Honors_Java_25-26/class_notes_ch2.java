9c5601d95f490da05ef810abb537095508ced85e
sanhith vandara <sanhith1211@gmail.com>
2026-08-19 16:04:35 -0400

    Add class notes for Chapter 2 in Java
    Corrected variable names and added comments for clarity.

---
+++ Honors_Java_25-26/class_notes_ch2.java
@@ -0,0 +1,68 @@
+/*Sanhith Vandara
+october 2, 2025 
+chapter 2- class notes */
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      //declare and initalize str with identifier  called 'name'
+      String name = "Sanhith" ; 
+      int num1;
+      double num2; 
+      
+      char grade = 'C';
+      
+      num11 = 67;
+      num22 = 3.14159;
+      //arithmetic operators 
+      //add
+      System.out.println(num11 + num22); 
+      //subtract
+      System.out.println(num11 - num22);
+      //multiply
+      System.out.println(num11 * num22);
+      //divide
+      System.out.println(num11 / num22);
+      //modulus
+      System.out.println(num22 % num11);
+      
+      //Java uses order of operations 
+      System.out.println(5+15+28+89);
+      //uses PEMDAS 
+      
+      
+    //char conversion 
+    System.out.println(name);
+    System.out.println(grade);
+    System.out.println(grade+10);
+    System.out.println(grade);
+    
+      
+      System.out.println(name);
+      
+      //print 'java'- J A V A using print functions
+      System.out.println( " J J J J J J     A A A    V        V    A A A "); 
+      System.out.println( "     J J        A     A    V      V    A     A ");
+      System.out.println( "     J J        A A A A     V    V     A A A A ");
+      System.out.println( "  J  J J       A      A      V  V      A      A ");
+      System.out.println( "  J   J        A      A       V V      A      A ");
+      System.out.println( "   J J         A      A        V       A      A  ");
+     
+     
+     // assignming values to variable 1 and 2 (num) 
+   num9 = 5;
+   num10 = 2.5;
+   //postfix  incremenet happens after the expression is executed 
+  
+    System.out.println(num1++);
+    ++num2;
+    //num1 increments to 6 
+    System.out.println(num1);
+    //num2 incremennts to 3.5 
+    System.out.println(num2);
+    // num compound 
+    
+  
+    }
+  }
